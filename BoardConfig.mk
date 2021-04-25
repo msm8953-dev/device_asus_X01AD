e3be79fa7dd18089ead8080f62e472af15e7b498
Gagan Malvi <malvi@aospa.co>
2021-04-25 15:48:39 +0530

    X01AD: Initial Paranoid Android bringup.
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,9 +108,6 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Filesystem
-TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
-
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
